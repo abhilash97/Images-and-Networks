c9b809392b56f1d8d9839aaef7ef57d5de4decbd
Abhilash <abhineog97@gmail.com>
2018-07-21 00:22:49 +0530

    First commit

---
+++ Edge detection/code/convolveUsingFilters.py
@@ -0,0 +1,33 @@
+#detecting edges using manual convolutions
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import io, color
+
+import matplotlib.image as mpimg
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+img = mpimg.imread(r'C:\Users\user\Desktop\Abhilash\Imp\Deep_Learning\Neural Networks\CNN\test2.jpg')     
+gray = rgb2gray(img)    
+plt.imshow(gray, cmap = plt.get_cmap('gray'))
+plt.show()
+
+from scipy import ndimage
+
+k1 = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])#for horizontal edges
+k2 = np.array([[1,0,-1],[1,0,-1],[1,0,-1]])#for vertical edges
+
+x = ndimage.convolve(gray, k1, mode='constant', cval=0.0)
+plt.imshow(x,cmap = plt.get_cmap('gray'))
+
+
+import scipy.signal
+
+edges = scipy.signal.convolve2d(gray, kernel, 'valid')
+# Adjust the contrast of the filtered image by applying Histogram Equalization
+edges_equalized = exposure.equalize_adapthist(edges/np.max(np.abs(edges)), clip_limit=0.09)
+plt.imshow(edges_equalized, cmap=plt.cm.gray)    # plot the edges_clipped
+plt.axis('off')
+plt.show()
