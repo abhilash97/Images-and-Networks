8a088ee9dfd695f1d46d6cb8eff7b02b1014fba0
Abhilash <abhineog97@gmail.com>
2018-08-02 22:24:54 +0530

    changes

--- Edge detection/convolveUsingFilters.py
+++ Edge detection/convolveUsingFilters.py
@@ -9,12 +9,15 @@ import matplotlib.image as mpimg
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-img = mpimg.imread(r'C:\Users\user\Desktop\Abhilash\Imp\Deep_Learning\Neural Networks\CNN\Images and Networks\Edge detection\Images\test2.jpg')     
-gray = rgb2gray(img)    
-plt.imshow(gray, cmap = plt.get_cmap('gray'))
-#plt.colorbar()
-#plt.show()
-plt.savefig('grayscale.pdf')
+def img_read():
+    img = mpimg.imread(r'C:\Users\user\Desktop\Abhilash\Imp\Deep_Learning\Neural Networks\CNN\Images and Networks\Edge detection\Images\test2.jpg')     
+    gray = rgb2gray(img)        
+    plt.imshow(gray, cmap = plt.get_cmap('gray'))
+    #plt.colorbar()
+    #plt.show()
+    plt.savefig('grayscale.pdf')
+    return gray
+
 
 k1 = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])#for horizontal edges
 k2 = np.array([[1,0,-1],[1,0,-1],[1,0,-1]])#for vertical edges
@@ -24,20 +27,23 @@ k2 = np.array([[1,0,-1],[1,0,-1],[1,0,-1]])#for vertical edges
 x = ndimage.convolve(gray, k1, mode='constant', cval=0.0)
 plt.imshow(x,cmap = plt.get_cmap('gray'))
 """
+def main():
+    import scipy.signal
+    from skimage import exposure
 
-import scipy.signal
-from skimage import exposure
+    gray = img_read()
+    edges = scipy.signal.convolve2d(gray, k1, 'valid')
+    # Adjust the contrast of the filtered image by applying Histogram Equalization
+    edges_equalized = exposure.equalize_adapthist(edges/np.max(np.abs(edges)), clip_limit=0.09)
+    plt.imshow(edges_equalized, cmap=plt.cm.gray)    # plot the edges_clipped
+    plt.axis('off')
+    #plt.show()
+    #plt.savefig('Horizontal edge detection.pdf')
 
-edges = scipy.signal.convolve2d(gray, k1, 'valid')
-# Adjust the contrast of the filtered image by applying Histogram Equalization
-edges_equalized = exposure.equalize_adapthist(edges/np.max(np.abs(edges)), clip_limit=0.09)
-#plt.imshow(edges_equalized, cmap=plt.cm.gray)    # plot the edges_clipped
-plt.axis('off')
-#plt.show()
-#plt.savefig('Horizontal edge detection.pdf')
+    #changing the filters into some random valued array
+    #kx = np.array([[1,2,3],[2,3,4],[1,6,7]])
+    #edgex = scipy.signal.convolve2d(gray, k1, 'valid')
+    #edges_equalizedx = exposure.equalize_adapthist(edgex/np.max(np.abs(edgex)), clip_limit=0.09)
+    #plt.imshow(edges_equalizedx, cmap=plt.cm.gray)    # plot the edges_clipped
 
-#changing the filters into some random valued array
-kx = np.array([[1,2,3],[2,3,4],[1,6,7]])
-edgex = scipy.signal.convolve2d(gray, k1, 'valid')
-edges_equalizedx = exposure.equalize_adapthist(edgex/np.max(np.abs(edgex)), clip_limit=0.09)
-plt.imshow(edges_equalizedx, cmap=plt.cm.gray)    # plot the edges_clipped
+
