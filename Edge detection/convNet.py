ed18a28760e0dcdf5aeb3c5a70513d76ddccae54
Abhilash <abhineog97@gmail.com>
2018-08-02 23:14:21 +0530

    changes

--- Edge detection/convNet.py
+++ Edge detection/convNet.py
@@ -1,17 +1,16 @@
 #visualizing the image within the covnet layers
-from keras.layers import Input, Dense
+from keras.layers import Input, Conv2D, MaxPooling2D
 from keras.models import Model
 
 import matplotlib.image as mpimg
 from convolveUsingFilters import img_read
 
 gray = img_read()
-print(gray)
-"""
-inputs = Input(shape=(784,))
+
+inputs = Input(shape=gray.shape)
 
 # a layer instance is callable on a tensor, and returns a tensor
-x = Dense(64, activation='relu')(inputs)
+x = Conv2D(16, kernel_size = (3,3), strides = (1,1),activation = 'relu')(inputs)
 x = Dense(64, activation='relu')(x)
 predictions = Dense(10, activation='softmax')(x)
 
